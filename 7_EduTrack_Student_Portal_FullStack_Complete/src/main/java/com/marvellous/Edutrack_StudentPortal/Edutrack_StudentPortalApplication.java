2696c5409549439b9ceeef7900d2ac6dcca79146
SurajGardi <surajgardi0707@gmail.com>
2026-01-11 17:42:51 +0530

    feat: finalize EduTrack backend with full CRUD and MongoDB integration

---
+++ 7_EduTrack_Student_Portal_FullStack_Complete/src/main/java/com/marvellous/Edutrack_StudentPortal/Edutrack_StudentPortalApplication.java
@@ -0,0 +1,15 @@
+package com.marvellous.Edutrack_StudentPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edutrack_StudentPortalApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Edutrack_StudentPortalApplication.class, args);
+	}
+
+}
---
+++ 7_EduTrack_Student_Portal_FullStack_Complete/src/main/java/com/marvellous/Edutrack_StudentPortal/controller/BatchEntryController.java
@@ -0,0 +1,66 @@
+package com.marvellous.Edutrack_StudentPortal.controller;
+
+import com.marvellous.Edutrack_StudentPortal.Entity.BatchEntry;
+import com.marvellous.Edutrack_StudentPortal.service.BatchEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/batches")
+public class BatchEntryController
+{
+    @Autowired
+    private BatchEntryService batchEntryService;
+
+    @GetMapping
+    public List<BatchEntry> getAll()
+    {
+        return batchEntryService.getAll();
+    }
+
+
+    @PostMapping
+    public String createEntry(@RequestBody BatchEntry myentry)
+    {
+        batchEntryService.saveEntry(myentry);
+        return "Document inserted Succesfully";
+    }
+
+
+//    @GetMapping("/id/{myid}")
+//    public Optional<BatchEntry> getBatchEntryById(@PathVariable ObjectId myid)
+//    {
+//        return batchEntryService.findById(myid);
+//    }
+
+    @GetMapping("/id/{myid}")
+    public BatchEntry getBatchEntryById(@PathVariable ObjectId myid)
+    {
+        return batchEntryService.findById(myid).orElse(null);
+    }
+
+
+    @DeleteMapping("/id/{myid}")
+    public boolean deleteEntryById(@PathVariable ObjectId myid)
+    {
+        batchEntryService.deleteByID(myid);
+        return true;
+    }
+
+
+    @PutMapping("/id/{myid}")
+    public void updateEntryById(@PathVariable ObjectId myid, @RequestBody BatchEntry myentry)
+    {
+        BatchEntry old = batchEntryService.findById(myid).orElse(null);
+        if(old != null)
+        {
+            old.setName(myentry.getName());
+            old.setFees(myentry.getFees());
+
+            batchEntryService.saveEntry(old);
+        }
+    }
+}
